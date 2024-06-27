9adca346f1aed390893bee0adb946f5a3ebe9322
vdviviana <vdviviana@hotmail.com>
2024-06-27 11:46:36 -0300

    Actualizacion

--- src/app.py
+++ src/app.py
@@ -1,14 +1,40 @@
+from flask import Flask
+from flask import render_template
+from flaskext.mysql import MySQL
 
-#from significa que llamo al modulo o librerias flask
-from flask import Flask #importo la clase Flask
-from flask import render_template # importo el metodo o funcion render_template
-from flaskext.mysql import MySQL #importo la clase MySQL
-
-#vamos a instanciar las clases
-app = Flask(__name__) #name es palabra reservada de Flask
+app = Flask(__name__)
 mysql = MySQL()
 
-if __name__ == 'main':
-    app.run(debug=True) #nos muestra errores por consola*
-    #ejecuto app #correr flask
 
+app.config['MYSQL_DATABASE_HOST']= 'localhost'
+app.config['MYSQL_DATABASE_USER']='root'
+app.config['MYSQL_DATABASE_PASSWORD']='root'
+app.config['MYSQL_DATABASE_DB']='empleados'
+
+mysql.init_app(app)
+#con un decorador
+#en la ruta raiz, cuando accedemos a localhost:5000
+#vamos a definir una funcion 
+@app.route('/')
+
+#----------
+# #empieza función index()
+def index():
+    sql= "Insert into empleados (nombre, correo, foto)  values ('Juan', 'juan@email.com','fotojuan.jpg');" 
+    #guardo string en variable sql
+
+    #conexión a la db
+    conn =mysql.connect()
+    cursor= conn.cursor()
+    #con el cursor me deja ejecutar query de la variable sql
+    cursor.execute(sql)
+    #envio petición
+    conn.commit()
+    #la funcion index() va a retornar un template html
+    return render_template('empleados/index.html')
+
+#termina funcion index()
+#----------
+
+if __name__ == '__main__':
+    app.run(debug=True)
